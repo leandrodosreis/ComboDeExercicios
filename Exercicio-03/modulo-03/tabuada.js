9bcfd0cfe14b85b7017f877e2a84400876118c96
leandrodosreisfilho-lang <leandrodosreisfilho@gmail.com>
2026-03-04 11:59:10 -0300

    tabuada

---
+++ Exercicio-03/modulo-03/tabuada.js
@@ -0,0 +1,27 @@
+/*
+Objetivo: Arquivo responsavel pelo processameto das tabuadas
+Data: 25/02/26
+Autor: Leandro
+Versão: 1.0
+*/
+
+const calcularTabuada = function(tabInicial, tabFinal, contInicial, contFinal){
+    tabInicial = Number(tabInicial)
+    tabFinal = Number(tabFinal)
+    contInicial = Number(contInicial)
+    contFinal = Number(contFinal)
+
+    for (let tab = tabInicial; tab <= tabFinal; tab++) {
+
+    console.log(`Tabuada do [${tab}]`)
+
+        
+        for (let cont = contInicial; cont <= contFinal; cont++) {
+            console.log(`${tab} x ${cont} = ${tab * cont}`)
+        }
+
+        console.log("------------------")
+    }
+}
+
+calcularTabuada(2, 3, 0, 2)
